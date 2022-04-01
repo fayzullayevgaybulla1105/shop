942c19681016aca08a8d3b21cebaaec2fc15cc42
fayzullayevgaybulla <fayzullayevgn@gmail.com>
2022-04-02 00:18:05 +0500

    header,footer,Product, rating .js files

---
+++ front/src/products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+        _id:"1",
+        name:"nokia",
+        image:"/images/nokia.jpg",
+        description:
+        "telefon xarakteristikasi",
+        brand:"nokia",
+        categoryy:"Electronics",
+        price:125.5,
+        countInStock:5,
+        rating:4.7,
+        numReviews:4
+    },
+    {
+        _id:"2",
+        name:"nokia",
+        image:"/images/nokia-best.jpeg",
+        description:
+        "telefon xarakteristikasi",
+        brand:"nokia",
+        categoryy:"Electronics",
+        price:125.5,
+        countInStock:5,
+        rating:5,
+        numReviews:4
+    },
+    {
+        _id:"3",
+        name:"iphone",
+        image:"/images/iphone.jpeg",
+        description:
+        "telefon xarakteristikasi",
+        brand:"iphone",
+        categoryy:"Electronics",
+        price:125.5,
+        countInStock:5,
+        rating:4.0,
+        numReviews:4
+    },
+    {
+        _id:"4",
+        name:"Mi",
+        image:"/images/OnePlus.jpg",
+        description:
+        "telefon xarakteristikasi",
+        brand:"Mi",
+        categoryy:"Electronics",
+        price:125.5,
+        countInStock:5,
+        rating:4.7,
+        numReviews:4
+    }
+]
+
+export default products
